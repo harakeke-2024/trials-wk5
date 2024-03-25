fc076bb682adf18a8966df7f61773125c778001f
Haruka <155341042+haruka-ogino@users.noreply.github.com>
2024-03-25 13:25:12 +1300

    getRandomInt refactored

--- client/components/RandomDishes.tsx
+++ client/components/RandomDishes.tsx
@@ -14,14 +14,19 @@ export default function RandomDishes() {
   const [max, setMax] = useState(4)
   const [randomInt, setRandomInt] = useState(2)
 
-  function getRandomDish() /*:<Number>*/ {
+  function getRandomInt() /*:<Number>*/ {
     const random = Math.floor(Math.random() * max + min)
     setRandomInt(random)
-    return random
+    return randomInt
   }
 
-  // random fn has been set up. Next, need to call it inside the return block to ensure it works.
+  // random fn has been set up. Next, need to call it inside the return block to ensure it works. -DONE
   // - add logic for figuring out the max position of the array
+
+  function getRandomDish() {
+    setRandomInt(getRandomInt())
+    return randomInt
+  }
 
   if (isLoading) return <h1>Loading...</h1>
 
@@ -32,9 +37,7 @@ export default function RandomDishes() {
       <div>
         <h2>Random Dish</h2>
         <p>under construction...</p>
-        <button onClick={() => setRandomInt(getRandomDish())}>
-          get random int
-        </button>
+        <button onClick={() => getRandomInt()}>get random int</button>
         <p>random int: {randomInt}</p>
       </div>
     )
