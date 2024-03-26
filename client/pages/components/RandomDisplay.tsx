0ad1e498705610379dec45380e78935c28f8bc45
Haruka Ogino <harukariq@gmail.com>
2024-03-26 21:27:03 +1300

    might be the double query call causing rerender

--- client/pages/components/RandomDisplay.tsx
+++ client/pages/components/RandomDisplay.tsx
@@ -1,15 +1,33 @@
+import { useQuery } from '@tanstack/react-query'
 import { CategoryWithFilm } from '../../../models/ghibli'
 import FilmsDisplay from './FilmsDisplay'
+import { differentFilm } from '../../apis/filmsApi'
 // import GetFilms from './GetFilms'
 
 interface Props {
-  dish: CategoryWithFilm | Record<string, never> | null | undefined
+  dish: CategoryWithFilm
 }
 
 export default function RandomDisplay({ dish }: Props) {
   console.log('answer below')
   console.log(dish)
-  if (dish) {
+  const id = dish?.filmId
+
+  const {
+    data: randomFilm,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['random-film', id],
+    queryFn: () => differentFilm(id),
+  })
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  if (isError) return <h1>Error; {error.message}</h1>
+
+  if (dish && randomFilm) {
     return (
       <>
         <p>-----------------------------------------------------------</p>
@@ -18,7 +36,10 @@ export default function RandomDisplay({ dish }: Props) {
         {/* <p>{dish.film}</p> */}
         <img src={dish.img} alt={dish.name} />
         <p>POSSIBLE FILM ANSWERS COMPONENT</p>
-        <FilmsDisplay film={dish.film} id={dish.filmId} />
+        <p>answer: {dish.film}</p>
+        <p>random film: {randomFilm.title}</p>
+
+        {/* <FilmsDisplay film={dish.film} id={dish.filmId} /> */}
         {/* <FilmsDisplay film={dish.film} /> */}
 
         {/* <GetFilms film={dish.film} /> */}
