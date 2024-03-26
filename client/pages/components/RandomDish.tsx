9ffcf110565f5f1a6afd08b8f8057043265b9ea7
Haruka Ogino <harukariq@gmail.com>
2024-03-27 11:50:06 +1300

    able to get new dishes at click of button

--- client/pages/components/RandomDish.tsx
+++ client/pages/components/RandomDish.tsx
@@ -1,6 +1,22 @@
+import { useQueryClient } from '@tanstack/react-query'
 import useRandomDishes from '../../hooks/useRandomDishes'
+// import { useState } from 'react'
+// import RandomChars from './components/RandomChars'
 
 export default function RandomDish() {
+  // const [category, setCategory] = useState('')
+  // const [counter, setCounter] = useState(0)
+
+  // function selectCategory() {
+  //   if (counter % 2 === 0) {
+  //     setCategory('dishes')
+  //   } else if (counter % 2 === 1) {
+  //     setCategory('characters')
+  //   }
+  //   setCounter((prevCounter) => prevCounter + 1)
+  // }
+
+  const queryClient = useQueryClient()
   const { data: dishes, isError, isLoading, error } = useRandomDishes()
 
   if (isLoading) return <h1>Loading...</h1>
@@ -10,7 +26,15 @@ export default function RandomDish() {
   // random fn has been set up. Next, need to call it inside the return block to ensure it works. -DONE
   // - add logic for figuring out the max position of the array
 
-  function handleGetCategoryItem() {}
+  function handleGetCategoryItem() {
+    // selectCategory()
+
+    getNewDishes()
+  }
+
+  function getNewDishes() {
+    queryClient.invalidateQueries({ queryKey: ['random'] })
+  }
 
   if (dishes) {
     console.log(dishes[0])
