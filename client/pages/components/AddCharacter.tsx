fcfd638a4acc6de6f7517f57ade772cb4ab73bc8
Haruka Ogino <harukariq@gmail.com>
2024-03-28 15:01:17 +1300

    form struggles

--- client/pages/components/AddCharacter.tsx
+++ client/pages/components/AddCharacter.tsx
@@ -1,9 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { addNewChar } from '../../apis/filmsApi.ts'
 import { useState } from 'react'
+import useFilmData from '../../hooks/useFilmData.ts'
+import { useNavigate } from 'react-router-dom'
 
 export default function AddCharacter() {
   const queryClient = useQueryClient()
+  const films = useFilmData()
+  const navigate = useNavigate()
 
   const [char, setChar] = useState({
     name: '',
@@ -17,7 +21,7 @@ export default function AddCharacter() {
       queryClient.invalidateQueries({ queryKey: ['characters'] })
       setChar({
         name: '',
-        film_id: 1,
+        film_id: 0,
         image_url: '',
       })
     },
@@ -29,38 +33,53 @@ export default function AddCharacter() {
     mutation.mutate(char)
   }
 
-  return (
-    <>
-      <h2>Add a new character below:</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="char-name">Name: </label>
-        <input
-          onChange={(e) => setChar({ ...char, name: e.target.value })}
-          type="text"
-          name="char-name"
-          id="char-name"
-          placeholder="Character Name"
-        />
-        <label htmlFor="char-film-id">Film: </label>
-        <input
-          onChange={(e) =>
-            setChar({ ...char, film_id: Number(e.target.value) })
-          }
-          type="text"
-          name="char-film-id"
-          id="char-film-id"
-          placeholder="As seen on"
-        />
-        <label htmlFor="char-img">Image Link: </label>
-        <input
-          onChange={(e) => setChar({ ...char, image_url: e.target.value })}
-          type="text"
-          name="char-img"
-          id="char-img"
-          placeholder="Character Image Link"
-        />
-        <button type="submit">Add Character</button>
-      </form>
-    </>
-  )
+  if (films.isLoading) <p>Loading film data...</p>
+
+  if (films.isError) <p>Failed to load film data</p>
+
+  if (films.data)
+    return (
+      <>
+        <h2>Add a new character below:</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="char-name">Name: </label>
+          <input
+            onChange={(e) => setChar({ ...char, name: e.target.value })}
+            type="text"
+            name="char-name"
+            id="char-name"
+            placeholder="Character Name"
+          />
+
+          <label htmlFor="film-id">Select Film: </label>
+          <select
+            id="film-id"
+            name="film-id"
+            onChange={(e) =>
+              setChar({ ...char, film_id: Number(e.target.value) })
+            }
+            value={char.film_id}
+          >
+            {films.data?.map((film) => (
+              <option key={film.id} value={film.id}>
+                {film.title}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="char-img">Image Link: </label>
+          <input
+            onChange={(e) => setChar({ ...char, image_url: e.target.value })}
+            type="text"
+            name="char-img"
+            id="char-img"
+            placeholder="Character Image Link"
+          />
+          <button type="submit">
+            {/* // onClick={() => navigate('/characters')}> */}
+            Add Character
+          </button>
+        </form>
+      </>
+    )
 }
