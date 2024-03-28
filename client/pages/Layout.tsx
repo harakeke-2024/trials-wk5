9b1b6a17f1e2c505d35db1003aef98248ec197b4
Haruka Ogino <harukariq@gmail.com>
2024-03-28 14:23:06 +1300

    alt txt to decor img added as it is required to describe functionality

--- client/pages/Layout.tsx
+++ client/pages/Layout.tsx
@@ -7,8 +7,11 @@ export default function Layout() {
       <header className="head-container">
         <h1>All the Ghibli Things!</h1>
         <Link to="/">
-          {/* eslint-disable-next-line jsx-a11y/alt-text*/}
-          <img src="https://www.ghibli-museum.jp/en/img/mainimg.png" alt="" />
+          {/* img needs alt text despite being decorative as it is providing functionality (link to home page) */}
+          <img
+            src="https://www.ghibli-museum.jp/en/img/mainimg.png"
+            alt="link to home page"
+          />
         </Link>
       </header>
       <main>
