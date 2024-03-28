34681060065cc822f6db106b141b4c8f1b4cf76e
Haruka Ogino <harukariq@gmail.com>
2024-03-28 21:48:18 +1300

    edit char form updated to match addChar form

--- client/pages/EditCharacter.tsx
+++ client/pages/EditCharacter.tsx
@@ -1,18 +1,21 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { editChar } from '../apis/filmsApi.ts'
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import useFilmData from '../hooks/useFilmData.ts'
 
 export default function EditCharacter() {
   const params = useParams()
   const id = Number(params.id)
+  const films = useFilmData()
+  const navigate = useNavigate()
 
   const queryClient = useQueryClient()
 
   const [char, setChar] = useState({
     id: id,
     name: '',
-    film_id: 0,
+    film_id: 1,
     image_url: '',
   })
 
@@ -20,12 +23,14 @@ export default function EditCharacter() {
     mutationFn: editChar,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['characters'] })
+      alert(`${char.name} has been edited`)
       setChar({
         id: id,
         name: '',
-        film_id: 0,
+        film_id: 1,
         image_url: '',
       })
+      navigate('/characters')
     },
   })
 
@@ -35,19 +40,31 @@ export default function EditCharacter() {
     mutation.mutate(char)
   }
 
-  return (
-    <>
-      <h2>Edit character below:</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="char-name">Name: </label>
-        <input
-          onChange={(e) => setChar({ ...char, name: e.target.value })}
-          type="text"
-          name="char-name"
-          id="char-name"
-          placeholder="Character Name"
-        />
-        <label htmlFor="char-film-id">Film: </label>
+  const handleFilmChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+    setChar((prevChar) => ({
+      ...prevChar,
+      film_id: Number(e.target.value),
+    }))
+  }
+
+  if (films.isLoading) <p>Loading film data...</p>
+
+  if (films.isError) <p>Failed to load film data</p>
+
+  if (films.data)
+    return (
+      <>
+        <h2>Edit character below:</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="char-name">Name: </label>
+          <input
+            onChange={(e) => setChar({ ...char, name: e.target.value })}
+            type="text"
+            name="char-name"
+            id="char-name"
+            placeholder="Character Name"
+          />
+          {/* <label htmlFor="char-film-id">Film: </label>
         <input
           onChange={(e) =>
             setChar({ ...char, film_id: Number(e.target.value) })
@@ -56,17 +73,32 @@ export default function EditCharacter() {
           name="char-film-id"
           id="char-film-id"
           placeholder="As seen on"
-        />
-        <label htmlFor="char-img">Image Link: </label>
-        <input
-          onChange={(e) => setChar({ ...char, image_url: e.target.value })}
-          type="text"
-          name="char-img"
-          id="char-img"
-          placeholder="Character Image Link"
-        />
-        <button type="submit">Update Character</button>
-      </form>
-    </>
-  )
+        /> */}
+
+          <label htmlFor="film-id">Select Film: </label>
+          <select
+            id="film-id"
+            name="film-id"
+            onChange={handleFilmChange}
+            value={char.film_id}
+          >
+            {films.data?.map((film) => (
+              <option key={film.id} value={film.id}>
+                {film.title}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="char-img">Image Link: </label>
+          <input
+            onChange={(e) => setChar({ ...char, image_url: e.target.value })}
+            type="text"
+            name="char-img"
+            id="char-img"
+            placeholder="Character Image Link"
+          />
+          <button type="submit">Update Character</button>
+        </form>
+      </>
+    )
 }
